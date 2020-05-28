c4c0aa0dd3210a336543b5f77a92233ab4e6c831
Clint Hanson <anthony.c.hanson.jr@saic.com>
2020-05-28 11:46:58 -0500

    Added simple_web_project and gitignore

---
+++ simple_web_app/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="simple_web_app.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <span id='connection_string'><%= connectionString %></span>
+        </div>
+    </form>
+</body>
+</html>
---
+++ simple_web_app/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace simple_web_app
+{
+  public partial class Default : System.Web.UI.Page
+  {
+    public string connectionString { get; private set; }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString.ToString();
+      String pageTitle = System.Configuration.ConfigurationManager.AppSettings["PageTitle"].ToString();
+      Page.Title = string.Format(pageTitle + " :: {0:d}", DateTime.Now);
+    }
+  }
+}
